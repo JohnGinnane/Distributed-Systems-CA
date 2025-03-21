0e2cef3d425ca4d9c6838b72204a21aff6890d28
John Ginnane <John.Ginnane1@Gmail.com>
2025-03-21 18:14:00 +0000

    Discovery boilerplate

--- discovery-service/main.js
+++ discovery-service/main.js
@@ -1,1 +1,20 @@
-console.log("hi from discovery!");
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
+const path = require("path");
+
+const PROTO_PATH = path.join(__dirname, "protos/discovery.proto");
+const packageDefinition = protoLoader.loadSync(PROTO_PATH);
+const discoveryProto = grpc.loadPackageDefinition(packageDefinition).discovery;
+
+const services = [];
+
+const server = new grpc.Server();
+
+// server.addService(discoveryProto.DiscoveryService.service, {
+//     // ServiceFunction: jsFunction
+// });
+
+server.bindAsync("127.0.0.1:50000", grpc.ServerCredentials.createInsecure(), () => {
+    console.log("Discovery Service running on 127.0.0.1:50000");
+    server.start();
+})
