443745247f6e99dcc3d0ffe6731d448db934b7a6
John Ginnane <John.Ginnane1@Gmail.com>
2025-03-21 18:32:25 +0000

    Connecting robot to discovery service

--- robot-service/main.js
+++ robot-service/main.js
@@ -2,14 +2,30 @@ const grpc = require("@grpc/grpc-js");
 const protoLoader = require("@grpc/proto-loader");
 const path = require("path");
 
-const PROTO_PATH = path.join(__dirname, "protos/robot.proto");
-const packageDefinition = protoLoader.loadSync(PROTO_PATH);
-const robotProto = grpc.loadPackageDefinition(packageDefinition).robot;
+// We need to load discovery proto so this robot service and register itself
+const discoveryProto = grpc.loadPackageDefinition(protoLoader.loadSync(path.join(__dirname, "protos/discovery.proto"))).discovery;
+const robotProto     = grpc.loadPackageDefinition(protoLoader.loadSync(path.join(__dirname, "protos/robot.proto"))).robot;
 
+let DISCOVERY_ADDRESS = "127.0.0.1:50000";
 let ADDRESS = "127.0.0.1";
-let PORT    = "50000";
+let PORT    = "50001";
 
 function addr() { return `${ADDRESS}:${PORT}`; }
+
+const discoveryService = new discoveryProto.DiscoveryService(DISCOVERY_ADDRESS, grpc.credentials.createInsecure());
+discoveryService.registerService({
+    serviceName: "robot",
+    serviceAddress: addr()
+}, (error, response) => {
+    if (error) {
+        console.log("An error occurred trying to register with discovery service: ");
+        console.log(error);
+        return;
+    } else {
+        console.log("Response from discovery service: ");
+        console.log(response);
+    }
+});
 
 const server = new grpc.Server();
 
