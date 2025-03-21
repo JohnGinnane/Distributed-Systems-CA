3bdc9fc00afd4a61c9123fbfd88ed747820c19ed
John Ginnane <John.Ginnane1@Gmail.com>
2025-03-21 18:19:17 +0000

    Work on discovery proto

--- discovery-service/main.js
+++ discovery-service/main.js
@@ -6,15 +6,29 @@ const PROTO_PATH = path.join(__dirname, "protos/discovery.proto");
 const packageDefinition = protoLoader.loadSync(PROTO_PATH);
 const discoveryProto = grpc.loadPackageDefinition(packageDefinition).discovery;
 
+let ADDRESS = "127.0.0.1";
+let PORT    = "50000";
+
+function addr() { return `${ADDRESS}:${PORT}`; }
+
 const services = [];
 
 const server = new grpc.Server();
 
-// server.addService(discoveryProto.DiscoveryService.service, {
-//     // ServiceFunction: jsFunction
-// });
+function registerService() {
 
-server.bindAsync("127.0.0.1:50000", grpc.ServerCredentials.createInsecure(), () => {
-    console.log("Discovery Service running on 127.0.0.1:50000");
-    server.start();
+}
+
+function unregisterService() {
+    
+}
+
+server.addService(discoveryProto.DiscoveryService.service, {
+    RegisterService: registerService,
+    UnregisterService: unregisterService
+});
+
+server.bindAsync(addr(), grpc.ServerCredentials.createInsecure(), () => {
+    console.log("Discovery Service running on " + addr());
+    //server.start(); // No longer necessary to call this function, according to node
 })
