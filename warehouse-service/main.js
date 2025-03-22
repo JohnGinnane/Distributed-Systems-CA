a2dbd28f02f8bfa490e6ee4f50c1be469b5be135
John Ginnane <John.Ginnane1@Gmail.com>
2025-03-22 00:02:49 +0000

    Set up warehouse controller base

--- warehouse-service/main.js
+++ warehouse-service/main.js
@@ -2,13 +2,11 @@ const grpc        = require("@grpc/grpc-js");
 const protoLoader = require("@grpc/proto-loader");
 const path        = require("path");
 
-// We need to load discovery proto so this robot service and register itself
 const discoveryProto = grpc.loadPackageDefinition(protoLoader.loadSync(path.join(__dirname, "protos/discovery.proto"))).discovery;
-const robotProto     = grpc.loadPackageDefinition(protoLoader.loadSync(path.join(__dirname, "protos/robot.proto"))).robot;
 
 let DISCOVERY_ADDRESS = "127.0.0.1:50000";
 let ADDRESS           = "127.0.0.1";
-let PORT              = "50100";
+let PORT              = "50001";
 let serviceID         = "";
 
 const discoveryService = new discoveryProto.DiscoveryService(DISCOVERY_ADDRESS, grpc.credentials.createInsecure());
@@ -16,7 +14,7 @@ const discoveryService = new discoveryProto.DiscoveryService(DISCOVERY_ADDRESS, 
 function address() { return `${ADDRESS}:${PORT}`; }
 
 discoveryService.registerService({
-    serviceName: "robot",
+    serviceName: "warehouse",
     serviceAddress: address()
 }, (error, response) => {
     if (error) {
@@ -35,7 +33,7 @@ discoveryService.registerService({
         // });
 
         server.bindAsync(address(), grpc.ServerCredentials.createInsecure(), () => {
-            console.log("Robot Service running on " + address());
+            console.log("Warehouse Service running on " + address());
             //server.start(); // No longer necessary to call this function, according to node
         })
     }
